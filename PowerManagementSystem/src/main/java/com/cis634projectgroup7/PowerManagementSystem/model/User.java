27fc8d6b180d6924085f13183910e2e0dab478ff
Mitee Patel <mkpatel386@gmail.com>
2022-11-06 20:57:59 -0500

    Initial Power Management System

---
+++ PowerManagementSystem/src/main/java/com/cis634projectgroup7/PowerManagementSystem/model/User.java
@@ -0,0 +1,63 @@
+package com.cis634projectgroup7.PowerManagementSystem.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int User_Id;
+	private String Name;
+	private String Password;
+	private String Email;
+	private String Address;
+	
+	public User() {
+	}
+
+	public int getUser_Id() {
+		return User_Id;
+	}
+
+	public void setUser_Id(int user_Id) {
+		User_Id = user_Id;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public String getPassword() {
+		return Password;
+	}
+
+	public void setPassword(String password) {
+		Password = password;
+	}
+
+	public String getEmail() {
+		return Email;
+	}
+
+	public void setEmail(String email) {
+		Email = email;
+	}
+
+	public String getAddress() {
+		return Address;
+	}
+
+	public void setAddress(String address) {
+		Address = address;
+	}
+	
+	
+}
