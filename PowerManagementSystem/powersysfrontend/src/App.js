742e14569d85431f28e392f424c37ff615abc6d8
Shaishav23 <66006866+Shaishav23@users.noreply.github.com>
2022-11-09 19:15:42 -0500

    Redid the whole Registration page

--- PowerManagementSystem/powersysfrontend/src/App.js
+++ PowerManagementSystem/powersysfrontend/src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
-import Appbar from './components/Appbar';
-import User from './components/User';
+import Navbar from './components/Navbar';
+import RegisterUser from './components/RegisterUser';
 
 function App() {
   return (
-    <div className="App">
-      <Appbar/>
-      <User/>
-    </div>
+    <>
+      <Navbar />
+      <RegisterUser />
+    </>
   );
 }
 
 export default App;
+
+
+
+
+// #configuration
+// spring.datasource.url=jdbc:mysql://localhost:3306/electricitymanagementsystem
+// spring.datasource.username=root
+// spring.datasource.password=
+// spring.datasource.driver-class-name =com.mysql.cj.jdbc.Driver
+// spring.jpa.hibernate.ddl-auto=update
+// spring.jpa.show-sql= true
