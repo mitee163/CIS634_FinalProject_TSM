37ce02bbfd901d213d5d7ae8a2d12a8dba69e2df
Shaishav23 <66006866+Shaishav23@users.noreply.github.com>
2022-11-13 21:51:53 -0500

    Update UserController.java

--- PowerManagementSystem/src/main/java/com/cis634projectgroup7/PowerManagementSystem/controller/UserController.java
+++ PowerManagementSystem/src/main/java/com/cis634projectgroup7/PowerManagementSystem/controller/UserController.java
@@ -24,7 +24,7 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
-	@GetMapping("/getAll")
+	@GetMapping("/users")
 	public List<User> getAllUsers(){
 		return userService.getAllUsers();
 	}
